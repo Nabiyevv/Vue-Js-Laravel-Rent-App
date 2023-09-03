f3cffd13ab885bf34a2aeb95c2d21552338c008d
Nabiyevv <maqa.nabiev@gmail.com>
2023-09-03 13:06:31 +0400

    Deleting some Comments

--- resources/js/src/components/NavBar.vue
+++ resources/js/src/components/NavBar.vue
@@ -13,17 +13,7 @@
         <div class="flex justify-between gap-5 items-center">
             <RentButton />
             <div class="flex">
-                <!-- Login page show and button -->
-                <!-- v-if="!token" -->
-
                 <div v-if="token" class="flex gap-5">
-                    <!-- <div
-                        @click="isVisible = !isVisible"
-                        class="pointer text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                    >
-                        Profile
-                        {{ console.log(isVisible) }}
-                    </div> -->
                     <button
                         @click="isVisible = !isVisible"
                         class="relative z-10 flex items-center p-2 text-sm text-gray-600 bg-gray-100 border border-transparent rounded-md focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:text-white dark:bg-gray-800 focus:outline-none"
@@ -42,26 +32,14 @@
                         </svg>
                     </button>
                     <SettignsButton v-if="isVisible" />
-                    <!-- <div @click="userStore.userData()">
-                        userData
-                    </div> -->
                 </div>
             </div>
-            <!-- <div @click="testReq">click</div> -->
-            <!-- <div>
-                <button
-                    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                    @click="toggleTheme"
-                >
-                    {{ isDark ? "Dark" : "Light" }}
-                </button>
-            </div> -->
         </div>
     </nav>
 </template>
 
 <script>
-import { useDark, useToggle } from "@vueuse/core";
+import { useDark } from "@vueuse/core";
 import SearchInput from "./SearchInput.vue";
 import RentButton from "./Auth/RentButton.vue";
 import { mapState } from "pinia";
