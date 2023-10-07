8a8c8b6af57e7a2ea11aac0afd18eadfefeda2a2
Nabiyevv <maqa.nabiev@gmail.com>
2023-10-07 23:30:41 +0400

    reload after login and logout

--- resources/js/src/store/user.js
+++ resources/js/src/store/user.js
@@ -21,10 +21,10 @@ export const useUserStore = defineStore({
                         'Accept' : 'application/json',
                         'Content-Type': 'application/json',
                     },
-                    // withCredentials:true,
                     credentials: 'include',
                 })
-                .then(async response => {                    
+                .then(async response => {
+                    window.location.reload();                    
                     this.token = response.data.token;
                     this.user = response.data.userData;
                     localStorage.setItem('user_data',JSON.stringify(this.user));
@@ -38,7 +38,7 @@ export const useUserStore = defineStore({
                 return formErrors;    
         },
         async register(formData) {
-            let formErrors ={};
+            let formErrors = {};
             await axios.post('/api/register',formData)
             .then((response)=>{
                 console.log(response);
@@ -50,19 +50,17 @@ export const useUserStore = defineStore({
             return formErrors;
         },
         async logout() {
-            // axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
             await axios.post('http://localhost:8000/api/logout',{},{
                 headers:{
                     "Authorization":`Bearer ${this.token}`
                 }
             })
             .then(response => {
-                console.log(response.data);
+                window.location.reload();
                 this.token = '';
                 this.user = null;
                 localStorage.removeItem('token');
                 localStorage.removeItem('user_data');
-                window.location.reload();
             })
             .catch(error => {
                 console.log(error);
@@ -77,8 +75,6 @@ export const useUserStore = defineStore({
             })
             .then(response => {
                 user = response.data;
-                // console.log(response.data);
-             
             })
             .catch(error => {
                 console.log(error);
@@ -86,8 +82,7 @@ export const useUserStore = defineStore({
             return user;
         },
         async updateUser(formData){
-            // console.log("🚀 ~ file: user.js:87 ~ updateUser ~ formData:", formData);
-            let formErrors ={};
+            let formErrors = null;
             await axios.post('http://localhost:8000/api/user',{'name':formData?.name,'contact':formData?.contact,'avatar':formData?.avatar},{
                 headers:{
                     'Accept' : 'application/json',
@@ -96,12 +91,10 @@ export const useUserStore = defineStore({
                 }
             })
             .then(response => {
-                console.log("🚀 ~ file: user.js:95 ~ updateUser ~ response:", response)
                 this.user = response.data[0];
                 localStorage.setItem('user_data',JSON.stringify(this.user));
             })
             .catch(error => {
-                // console.log(error.response.data.errors);
                 formErrors = error.response.data.errors;
             });
             return formErrors;
